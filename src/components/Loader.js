342aa58e1e8182e53d75b19a18f90e5824bb6a71
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-10-20 15:01:56 +0200

    loading state

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import { Html} from '@react-three/drei'
 const CustomModel = () => {
 
   return (
-  <Html center><p>Loading presentation...</p></Html>
+  <Html center><p className={"loader"}>Loading presentation...</p></Html>
   );
 }
 
